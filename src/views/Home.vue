a2169b2024a0900cc53507f2fdeeefccc161883f
luqman00 <muhammad.shubkhi@students.amikom.ac.id>
2020-04-12 17:58:07 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderToko />
+    <HeroToko />
+    <BannerToko />
+    <InstagramToko />
+    <LogoPartner />
+    <FooterToko />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+ import HeaderToko from '@/components/HeaderToko.vue';
+ import HeroToko from '@/components/HeroToko.vue';
+ import BannerToko from '@/components/BannerToko.vue';
+ import InstagramToko from '@/components/InstagramToko.vue';
+ import LogoPartner from '@/components/LogoPartner';
+ import FooterToko from '@/components/FooterToko';
+
+export default {
+  name: 'Home',
+  components: {
+      HeaderToko,
+      HeroToko,
+      BannerToko,
+      InstagramToko,
+      LogoPartner,
+      FooterToko,
+  }
+};
+</script>
